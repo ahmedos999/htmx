9b5d142de321b19bd6b5b103e4162cd4160675b8
Ahmed Sheikh <ahmedos099912@gmail.com>
2024-08-07 22:57:06 +0400

    htmx first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+import express from 'express'
+import createHomePageTemplate from './views/index.js'
+import createListTemplate from './views/list.js';
+
+const app = express();
+app.use(express.urlencoded({extended:false}))
+
+
+app.use(express.static('public'))
+
+app.get('/',(req,res)=>{
+    res.send(createHomePageTemplate());
+})
+
+app.get('/books',(req,res)=>{
+    res.send(createListTemplate())
+})
+
+app.listen(3000,()=>{
+    console.log('App listening on port 3000')
+})
