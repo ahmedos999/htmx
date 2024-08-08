f4f8ade030719dd85cdf41e168a6e685f967e9a8
Ahmed Sheikh <ahmedos099912@gmail.com>
2024-08-08 05:40:56 +0400

    add book

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 import express from 'express'
+
+
 import createHomePageTemplate from './views/index.js'
 import createListTemplate from './views/list.js';
+import createBookTemplate from './views/book.js';
+
+
+import BOOKS_DATA from './data/data.js';
+
 
 const app = express();
 app.use(express.urlencoded({extended:false}))
@@ -14,6 +21,23 @@ app.get('/',(req,res)=>{
 
 app.get('/books',(req,res)=>{
     res.send(createListTemplate())
+})
+
+app.post('/books',(req,res)=>{
+    const {title,author} = req.body
+    const id = Math.random().toString()
+
+    BOOKS_DATA.push({id,title,author})
+
+    res.redirect(`/books/${id}`)
+})
+
+app.get('/books/:id',(req,res)=>{
+    const {id} = req.params
+
+    const book = BOOKS_DATA.find((b)=>b.id === id)
+
+    res.send(createBookTemplate(book))
 })
 
 app.listen(3000,()=>{
